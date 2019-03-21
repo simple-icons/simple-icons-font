cd8d05a0197198dddc0224ab2df4755ec23ed17c
Metaa <git@metaa.io>
2019-03-21 20:27:27 +0100

    Added the first iteration of the font generation script.

---
+++ scripts/build.js
@@ -0,0 +1,39 @@
+// core packages
+const path = require('path')
+
+// npm packages
+const SimpleIcons = require('simple-icons'),
+      IconFontBuildr = require('icon-font-buildr')
+
+// utils
+const { titleToFilename } = require('../lib/utils')
+
+// script
+const builder = new IconFontBuildr({
+	sources: [
+		path.join(path.dirname(require.resolve('simple-icons')), 'icons', '[icon].svg')
+	],
+	icons: Object.values(SimpleIcons).map(icon => ({
+		icon: titleToFilename(icon.title), // source file name, e.g. "about-dot-me"
+		ligatures: [ // strings that get replaced with the icon in this font
+			icon.title.replace(/[ !’]/g, ''),                  // "About.me"
+			icon.title.replace(/[ !’]/g, '').toLowerCase(),    // "about.me"
+			titleToFilename(icon.title)                        // "about_dot_me"
+		].filter((v, i, a) => a.indexOf(v) === i) // and filtering duplicates
+	})),
+	output: {
+		ligatures: true,
+		icons: null,
+		fonts: path.join(__dirname, '..', 'font'),
+		fontName: 'SimpleIcons',
+		formats: [
+			'woff',
+			'woff2'
+		]
+	}
+})
+
+module.exports =  builder.build()
+	.then(() => {
+		console.log('Font created successfully!')
+	})
