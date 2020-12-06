47eb17cccb04bdad0aedb433fad75858f789868f
Eric Cornelissen <ericornelissen@gmail.com>
2020-12-07 00:10:14 +0100

    Logic for CSS class generation to use for icons (#39)
    * Added logic to generate CSS classes that spawn icons.
    
    * Use titleToFilename for CSS class name
    
    To reduce code duplication and to increase consistency in naming between 
    the main package and this font.
    
    * Make the build script build the CSS and font instead of the testpage
    
    ... and the test page run the build script and the buildTestpage.js 
    script
    
    * Removed building of the test page in the "build" script.
    
    * Add postpublish and clean scripts
    
    Co-authored-by: Metaa <git@metaa.io>

---
+++ scripts/utils.js
@@ -0,0 +1,27 @@
+module.exports = {
+  /**
+   * Converts a brand title into a filename (not a full path)
+   * @param {String} title The title to convert
+   */
+  titleToFilename: title => (
+    title.toLowerCase()
+      .replace(/\+/g, "plus")
+      .replace(/^\./, "dot-")
+      .replace(/\.$/, "-dot")
+      .replace(/\./g, "-dot-")
+      .replace(/^&/, "and-")
+      .replace(/&$/, "-and")
+      .replace(/&/g, "-and-")
+      .replace(/[ !:’']/g, "")
+      .replace(/à|á|â|ã|ä/g, "a")
+      .replace(/ç|č|ć/g, "c")
+      .replace(/è|é|ê|ë/g, "e")
+      .replace(/ì|í|î|ï/g, "i")
+      .replace(/ñ|ň|ń/g, "n")
+      .replace(/ò|ó|ô|õ|ö/g, "o")
+      .replace(/š|ś/g, "s")
+      .replace(/ù|ú|û|ü/g, "u")
+      .replace(/ý|ÿ/g, "y")
+      .replace(/ž|ź/g, "z")
+  )
+}
