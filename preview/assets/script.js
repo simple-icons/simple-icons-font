5cdd2e48fc980f8b74acb257f54a204e6c70486b
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-12-07 01:11:17 +0100

    Add black/colored icons toggler to preview page

--- preview/assets/script.js
+++ preview/assets/script.js
@@ -1,5 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-	document.querySelector('button.toggle').addEventListener('click', () => {
-		document.querySelector('body').classList.toggle('dark')
+	// DOM elements
+	const body = document.querySelector('body'),
+	      icons = document.getElementsByClassName('simpleicons');
+
+	// Buttons
+	const backgroundModeButton = document.querySelector('button.background-mode'),
+	      iconsColorButton = document.querySelector('button.icons-color');
+
+	// Background dark/light mode toggler
+	backgroundModeButton.addEventListener('click', () => {
+		if (body.classList.contains('dark')) {
+			backgroundModeButton.innerText = 'Dark background';
+		} else {
+			backgroundModeButton.innerText = 'Light background';
+		}
+		body.classList.toggle('dark');
+	})
+
+	// Icons black/colored toggle
+	iconsColorButton.addEventListener('click', () => {
+		if (icons[0].classList.contains('simpleicons--color')) {
+			iconsColorButton.innerText = 'Colored icons';
+		} else {
+			iconsColorButton.innerText = 'Black icons';
+		}
+
+		for (let i=0; i<icons.length; i++) {
+			icons[i].classList.toggle('simpleicons--color');
+		}
 	})
 })
