3f69dd41e6a0445a93c7adf8c6734cd98450efcd
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-12-09 14:50:25 +0100

    Use backticks for strings that includes icons CSS

--- scripts/build.js
+++ scripts/build.js
@@ -62,9 +62,9 @@ const buildSimpleIconsCssFile = (unicodeHexBySlug) => {
     
     // &#xF26E; -> \f26e
     const cssDecodedUnicode = icon.unicode.replace('&#x', '\\').replace(';', '').toLowerCase();
-    simpleIconsCss += '\n'
-                    +   `.simpleicons-${slug}::before { content: "${cssDecodedUnicode}"; }\n`
-                    +   `.simpleicons-${slug}.simpleicons--color::before { color: #${icon.hex}; }\n`;
+    simpleIconsCss += `
+.simpleicons-${slug}::before { content: "${cssDecodedUnicode}"; }
+.simpleicons-${slug}.simpleicons--color::before { color: #${icon.hex}; }`;
   }
   
   const outputFilePath = path.join(DISTDIR, 'simple-icons.css');
