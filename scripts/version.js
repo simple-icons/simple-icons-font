4eca331dddf1e28fcd056c71a055389c08a32638
Eric Cornelissen <ericornelissen@gmail.com>
2021-01-07 19:42:26 +0100

    Update version number of this package

--- scripts/version.js
+++ scripts/version.js
@@ -1,11 +1,59 @@
-const exec = require('child_process').execSync;
+const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const semver = require('semver');
 
-async function main() {
+const PACKAGE_JSON_FILE = path.resolve(__dirname, '..', 'package.json');
+const PACKAGE_LOCK_FILE = path.resolve(__dirname, '..', 'package-lock.json');
+
+function stringifyJson(object) {
+  return JSON.stringify(object, null, 2);
+}
+
+ function updateSimpleIconsDependency() {
   try {
-    await exec('npm uninstall simple-icons');
-    await exec('npm install --save-dev --save-exact simple-icons');
+    execSync('npm uninstall simple-icons');
+    execSync('npm install --save-dev --save-exact simple-icons');
   } catch (err) {
     console.error('Failed to update simple-icons to latest version:', err);
+    return err;
+  }
+}
+
+function updateVersionNumber() {
+  try {
+    const packageFileRaw = fs.readFileSync(PACKAGE_JSON_FILE).toString();
+    const packageLockRaw = fs.readFileSync(PACKAGE_LOCK_FILE).toString();
+
+    const packageFile = JSON.parse(packageFileRaw);
+    const packageLock = JSON.parse(packageLockRaw);
+
+    const currentVersion = packageFile.version;
+    const simpleIconsVersion = packageFile.devDependencies['simple-icons'];
+    if (semver.gt(simpleIconsVersion, currentVersion)) {
+      packageFile.version = simpleIconsVersion;
+      packageLock.version = simpleIconsVersion;
+    } else {
+      throw new Error(`Version already exists (${currentVersion})`);
+    }
+
+    fs.writeFileSync(PACKAGE_JSON_FILE, stringifyJson(packageFile) + '\n');
+    fs.writeFileSync(PACKAGE_LOCK_FILE, stringifyJson(packageLock) + '\n');
+  } catch (err) {
+    console.error('Failed to version bump this package:', err);
+    return err;
+  }
+}
+
+function main() {
+  let err = updateSimpleIconsDependency();
+  if (err) {
+    return;
+  }
+
+  err = updateVersionNumber();
+  if (err) {
+    return;
   }
 }
 
