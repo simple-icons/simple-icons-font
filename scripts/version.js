1d4a872c51828a48b642b7e63b48ec54db3de6b3
Eric Cornelissen <ericornelissen@gmail.com>
2021-01-07 18:14:23 +0100

    Update version.js to update simple-icons to latest

--- scripts/version.js
+++ scripts/version.js
@@ -1,16 +1,12 @@
-// core packages
-const path = require('path');
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
-
-const packageJson = require('./../package.json');
+const exec = require('child_process').execSync;
 
 async function main() {
-  const { stdout, stderr } = await exec(
-    `npm i -E simple-icons@${packageJson.version}`
-  );
-
-  if (stderr) console.error(`error: ${stderr}`);
+  try {
+    await exec('npm uninstall simple-icons');
+    await exec('npm install --save-dev --save-exact simple-icons');
+  } catch (err) {
+    console.error('Failed to update simple-icons to latest version:', err);
+  }
 }
 
 main();
