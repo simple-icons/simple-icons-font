2a187847bb508bcf72a25b4c26654e5b6ea812e4
Eric Cornelissen <ericornelissen@gmail.com>
2021-01-09 02:10:43 +0100

    General refactoring for consistency (#86)
    * Refactor scripts for consistency
    
    * Refactor preview page script

--- scripts/build.js
+++ scripts/build.js
@@ -5,31 +5,31 @@
  * dependency.
  */
 
+const CleanCSS = require('clean-css');
 const fs = require('fs');
 const path = require('path');
-const util = require('util');
-
-const CleanCSS = require('clean-css');
 const { ucs2 } = require('punycode/');
 const SimpleIcons = require('simple-icons');
 const svg2ttf = require('svg2ttf');
 const SVGPath = require('svgpath');
 const ttf2woff = require('ttf2woff');
 const ttf2woff2 = require('ttf2woff2');
+const util = require('util');
 
 const packageJson = require('./../package.json');
 
 const UTF8 = 'utf8';
 
-const DISTDIR = path.resolve(__dirname, '..', 'font');
+const DIST_DIR = path.resolve(__dirname, '..', 'font');
 
-const CSS_OUTPUT_FILEPATH = path.join(DISTDIR, 'simple-icons.css');
-const CSS_MINIFIED_OUTPUT_FILEPATH = path.join(DISTDIR, 'simple-icons.min.css');
-const SVG_OUTPUT_FILEPATH = path.join(DISTDIR, 'SimpleIcons.svg');
-const TTF_OUTPUT_FILEPATH = path.join(DISTDIR, 'SimpleIcons.ttf');
-const WOFF_OUTPUT_FILEPATH = path.join(DISTDIR, 'SimpleIcons.woff');
-const WOFF2_OUTPUT_FILEPATH = path.join(DISTDIR, 'SimpleIcons.woff2');
+const CSS_OUTPUT_FILEPATH = path.join(DIST_DIR, 'simple-icons.css');
+const CSS_MIN_OUTPUT_FILEPATH = path.join(DIST_DIR, 'simple-icons.min.css');
+const SVG_OUTPUT_FILEPATH = path.join(DIST_DIR, 'SimpleIcons.svg');
+const TTF_OUTPUT_FILEPATH = path.join(DIST_DIR, 'SimpleIcons.ttf');
+const WOFF_OUTPUT_FILEPATH = path.join(DIST_DIR, 'SimpleIcons.woff');
+const WOFF2_OUTPUT_FILEPATH = path.join(DIST_DIR, 'SimpleIcons.woff2');
 
+const CSS_BASE_FILE = path.resolve(__dirname, 'templates', 'base.css');
 const SVG_TEMPLATE_FILEPATH = path.join(__dirname, 'templates', 'font.svg');
 
 const cssDecodeUnicode = (value) => {
@@ -38,10 +38,10 @@ const cssDecodeUnicode = (value) => {
 };
 
 const buildSimpleIconsSvgFontFile = () => {
-  let startUnicode = 0xea01,
-    glyphsContent = '';
-  const usedUnicodes = [],
-    unicodeHexBySlug = [];
+  const usedUnicodes = [];
+  const unicodeHexBySlug = [];
+  let startUnicode = 0xea01;
+  let glyphsContent = '';
 
   for (let iconTitle in SimpleIcons) {
     let nextUnicode = ucs2.decode(String.fromCodePoint(startUnicode++));
@@ -76,9 +76,7 @@ const buildSimpleIconsSvgFontFile = () => {
 };
 
 const buildSimpleIconsCssFile = (unicodeHexBySlug) => {
-  let cssFileContent = fs.readFileSync(
-    path.resolve(__dirname, '..', 'preview', 'css', 'base.css')
-  );
+  let cssFileContent = fs.readFileSync(CSS_BASE_FILE);
 
   for (let slug in unicodeHexBySlug) {
     let icon = unicodeHexBySlug[slug];
@@ -99,8 +97,8 @@ const buildSimpleIconsMinCssFile = (cssFileContent) => {
     compatibility: 'ie7',
   }).minify(cssFileContent);
 
-  fs.writeFileSync(CSS_MINIFIED_OUTPUT_FILEPATH, cssMinifiedFile.styles);
-  console.log(`'${CSS_MINIFIED_OUTPUT_FILEPATH}' file built`);
+  fs.writeFileSync(CSS_MIN_OUTPUT_FILEPATH, cssMinifiedFile.styles);
+  console.log(`'${CSS_MIN_OUTPUT_FILEPATH}' file built`);
 };
 
 const buildSimpleIconsTtfFontFile = (svgFileContent) => {
@@ -128,8 +126,8 @@ const buildSimpleIconsWoff2FontFile = (ttfFileContent) => {
 };
 
 const main = () => {
-  if (!fs.existsSync(DISTDIR)) {
-    fs.mkdirSync(DISTDIR);
+  if (!fs.existsSync(DIST_DIR)) {
+    fs.mkdirSync(DIST_DIR);
   }
 
   const { unicodeHexBySlug, svgFileContent } = buildSimpleIconsSvgFontFile();
