0e2e5ef7e83989d71baa68374a1fb5eb0a06d8a2
Eric Cornelissen <ericornelissen@gmail.com>
2021-02-03 22:43:08 +0100

    Normalize name of simpleIcons import

--- scripts/build.js
+++ scripts/build.js
@@ -9,7 +9,7 @@ const CleanCSS = require('clean-css');
 const fs = require('fs');
 const path = require('path');
 const { ucs2 } = require('punycode/');
-const SimpleIcons = require('simple-icons');
+const simpleIcons = require('simple-icons');
 const svg2ttf = require('svg2ttf');
 const SVGPath = require('svgpath');
 const ttf2woff = require('ttf2woff');
@@ -43,7 +43,7 @@ function buildSimpleIconsSvgFontFile() {
   let startUnicode = 0xea01;
   let glyphsContent = '';
 
-  for (let iconTitle in SimpleIcons) {
+  for (let iconTitle in simpleIcons) {
     let nextUnicode = ucs2.decode(String.fromCodePoint(startUnicode++));
     let unicodeString = nextUnicode
       .map((point) => `&#x${point.toString(16).toUpperCase()};`)
@@ -52,7 +52,7 @@ function buildSimpleIconsSvgFontFile() {
       throw Error(`Unicodes must be unique. Found '${unicodeString}' repeated`);
     }
 
-    const icon = SimpleIcons[iconTitle];
+    const icon = simpleIcons[iconTitle];
     const verticalTransformedPath = SVGPath(icon.path)
       .scale(50, -50)
       .round(6)
