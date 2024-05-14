1ea1e4200f7d66b5bf0ea67c54a3774b2f544d34
Aman9das <39594914+Aman9das@users.noreply.github.com>
2024-05-14 13:11:37 +0530

    Update build.js

--- scripts/build.js
+++ scripts/build.js
@@ -65,7 +65,7 @@ const buildSimpleIconsSvgFontFile = async () => {
 
     const icon = simpleIcons[si];
     const verticalTransformedPath = SVGPath(icon.path)
-      .translate(0, -24)
+      .translate(0, -26)
       .scale(50, -50)
       .round(6)
       .toString();
