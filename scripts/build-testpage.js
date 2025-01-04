403e33e7958768f168b5cce8491ba37bfee24b89
LitoMore <litomore@gmail.com>
2025-01-04 21:45:06 +0800

    Add support for custom builds

--- scripts/build-testpage.js
+++ scripts/build-testpage.js
@@ -6,8 +6,9 @@
 
 import fs from 'node:fs';
 import path from 'node:path';
+import process from 'node:process';
 import pug from 'pug';
-import { getIconsData, titleToSlug } from 'simple-icons/sdk';
+import { getIconsData, getIconSlug } from 'simple-icons/sdk';
 import { fileURLToPath } from 'node:url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -16,11 +17,16 @@ const ROOT_DIR = path.resolve(__dirname, '..');
 const INPUT_FILE = path.join(ROOT_DIR, 'preview', 'html', 'testpage.pug');
 const OUTPUT_FILE = path.join(ROOT_DIR, 'preview', 'testpage.html');
 
+const { SI_FONT_SLUGS_FILTER = '' } = process.env;
+const siFontSlugs = new Set(SI_FONT_SLUGS_FILTER.split(','));
+
 const iconsData = await getIconsData();
-const icons = iconsData.map((icon) => ({
-  title: icon.title,
-  slug: icon.slug || titleToSlug(icon.title),
-}));
+const icons = iconsData
+  .map((icon) => ({
+    title: icon.title,
+    slug: getIconSlug(icon),
+  }))
+  .filter((icon) => siFontSlugs.size === 0 || siFontSlugs.has(icon.slug));
 
 pug.renderFile(INPUT_FILE, { icons }, (renderError, html) => {
   if (renderError) {
