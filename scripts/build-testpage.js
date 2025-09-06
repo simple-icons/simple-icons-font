82c9179c9bcda7c8eb121a51f5e28577a594cb29
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2025-09-06 18:10:29 +0100

    Stop using the SDK to get icons `slug`s (#240)

--- scripts/build-testpage.js
+++ scripts/build-testpage.js
@@ -8,7 +8,6 @@ import fs from 'node:fs';
 import path from 'node:path';
 import process from 'node:process';
 import pug from 'pug';
-import { getIconsData, getIconSlug } from 'simple-icons/sdk';
 import { fileURLToPath } from 'node:url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -20,11 +19,15 @@ const OUTPUT_FILE = path.join(ROOT_DIR, 'preview', 'testpage.html');
 const { SI_FONT_SLUGS_FILTER = '' } = process.env;
 const siFontSlugs = new Set(SI_FONT_SLUGS_FILTER.split(',').filter(Boolean));
 
-const iconsData = await getIconsData();
+// TODO: on Node.js v24 -> `import icons from 'simple-icons/icons.json' with { type: 'json' }`
+const iconsData = JSON.parse(
+  fs.readFileSync('./node_modules/simple-icons/data/simple-icons.json', 'utf8'),
+);
+
 const icons = iconsData
   .map((icon) => ({
     title: icon.title,
-    slug: getIconSlug(icon),
+    slug: icon.slug,
   }))
   .filter((icon) => siFontSlugs.size === 0 || siFontSlugs.has(icon.slug));
 
